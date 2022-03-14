0254516285b49fe16502dac559e22c3ff8ef0075
Bigbalo29 <balogunlateef29@gmail.com>
2022-03-14 06:35:58 +0000

    my commit for 5th task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+int digit;
+
+for (i =0 ; i < 10; 1++)
+{
+printf("%i", i);
+}
+
+printf("\n");
+
+return (0);
+
+}
