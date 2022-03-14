343d924dff99cac2f4688cadb201d78cfecaca5e
Bigbalo29 <balogunlateef29@gmail.com>
2022-03-14 08:06:19 +0000

    my commit for 9th task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#incluude <time.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -20,7 +20,7 @@ if (n == 0)
 printf("%i is zero\n", n);
 }
 
-else if (n<0>)
+else if (n<0)
 {
 printf(%i is negative\n", n);
 }
